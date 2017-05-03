63336d5031c19861fd6571683406c41f8352ce83
joxpeng <joxpeng@163.com>
2017-05-03 10:13:06 +0800

    扇形图

--- study/src/main/java/com/zyp/study/MainActivity.java
+++ study/src/main/java/com/zyp/study/MainActivity.java
@@ -3,11 +3,24 @@ package com.zyp.study;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private PieView pieView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pieView = (PieView) findViewById(R.id.v_pie);
+
+        List<PieView.PieData> datas = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            PieView.PieData data = new PieView.PieData(""+i,((i % 2 == 0) ? i + 10f : i-10f));
+            datas.add(data);
+        }
+        pieView.setData((ArrayList<PieView.PieData>) datas);
     }
 }
